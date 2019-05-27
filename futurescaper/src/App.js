97ce1b1a7c29632baac80599b20b2ea02e40aea1
Heath Banner <God@Heaths-MBP.lan1>
2019-05-27 12:01:59 -0400

    Somewhat framework for group

---
+++ futurescaper/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-2"></div>
+        <div className="col-lg-8">
+          <div id="main-content">
+            <h1>Hello!</h1>
+          </div>
+        </div>
+        <div className="col-lg-2"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
