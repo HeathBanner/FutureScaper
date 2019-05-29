be7500ef02020a0f8796bd0cb7a91f4bb79ec373
Heath Banner <God@Heaths-MacBook-Pro.local>
2019-05-29 10:24:37 -0400

    fixed conflicts again

--- server/models/plant.js
+++ server/models/plant.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -21,27 +20,3 @@ const PlantSchema = new Schema({
 const Plant = mongoose.model('Plant', PlantSchema);
 
 module.exports = Plant;
-=======
-
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const PlantSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  lifespan: {
-    type: String,
-    required: true
-  },
-  bloom_period: {
-    type: String,
-    required: false
-  },
-});
-
-let Plant = mongoose.model("Plant", PlantSchema);
-
-module.exports = Plant;
->>>>>>> f4ec3da182d7484786105526c90048e8389aa3c3
