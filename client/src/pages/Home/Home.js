5be9d052d144b89ee4fdcee1b493bbe1078ad69c
Cody Swank <cody.swank1@gmail.com>
2019-06-03 13:39:28 -0400

    added layout

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import update from 'immutability-helper';
 import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
-import Homeinfo from "../../components/Home/homeinfo"
+import Homeinfo from "../../components/Home/homeinfo";
 import Example from '../../components/DnD/Example';
 import Jumbotron from '../../components/Home/jumbotron';
 import Welcome from '../../components/Home/welcome';
@@ -79,7 +79,7 @@ class HomePage extends Component {
 
         return (
           <div>
-                {/* <Example /> */}
+                <Example />
               <div className="row">
                 <Jumbotron />
               </div>
@@ -87,14 +87,14 @@ class HomePage extends Component {
                 <Homeinfo />
               </div>
 
-              {/* <div id="intro-container" className="item-container">
+              <div id="intro-container" className="item-container">
                 <Welcome />
                 <Intro />
-              </div> */}
-              {/* <div id="container-two">
+              </div>
+              <div id="container-two">
                 <PlantsHeader />
                 <PlantsBody />
-              </div> */}
+              </div>
                 <Footer />
               </div>
               
